a1050e8b376aee97eca4b82857501ff96d095859
ancienthoon <72641982+ancienthoon@users.noreply.github.com>
2020-10-10 14:30:04 +0900

    Add files via upload

---
+++ 20201010_바운스볼.py
@@ -0,0 +1,34 @@
+import pygame as p
+
+p.init()
+w = (255,255,255)
+size = (1000,600)
+sc = p.display.set_mode(size)
+p.display.set_caption("bounce_ball")
+playing = True
+
+ball = p.image.load("ball.png")
+b_rect = ball.get_rect(left=490,top=290)
+b_x = 0
+b_y = 0
+clock = p.time.Clock()
+
+while playing:
+       
+       
+       clock.tick(60)  
+       for event in p.event.get():
+              if event.type == p.QUIT:
+                   playing = False
+                   p.quit()
+                   quit()
+
+       sc.fill(w)
+       sc.blit(ball,b_rect)
+       b_rect.top += b_y
+       b_y = b_y + 1
+       if b_rect.top >= 590:
+              b_y = -20
+              
+       p.display.flip()
+       
